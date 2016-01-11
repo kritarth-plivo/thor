5ef8fc43dc0be83d9fca3f8c94273097b8ab144d
unknown <iorichina@gmail.com>
2016-01-11 11:05:46 +0800

    do not write buffer data to server when received msg from server while task.messages set to 0

--- worker-socketio.js
+++ worker-socketio.js
@@ -229,6 +229,7 @@ process.on('message', function message(task) {
     }, task);
 
     // Only write as long as we are allowed to send messages
+    if (task.messages > 0)
     if (--task.messages) {
       write(socket, task, task.id);
     } else {
